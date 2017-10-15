1b8c78fe6ec3a5a39bd581a2d0808b406ac1b9eb
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2017-10-15 19:02:17 +0100

    MAINT: cosmetic changes

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -11,21 +11,20 @@ def two_sum(nums, target):
         target (int): The target.
 
     Returns:
-        (list) The indexes of the elements that sum to the target.
+        (list) The indexes of the elements that sum to the target value.
     """
-    # use dictionary to search in O(1) complexity
+    # convert `target` to dictionary for fast search
     numbers = defaultdict(deque)
     for idx, number in enumerate(nums):
         numbers[number].append(idx)
 
     # find complement number to sum up to target
     for first_num in nums:
-        complementary_number = target - first_num
-        if complementary_number in numbers:
-            if complementary_number == first_num:
-                if len(numbers[first_num]) < 2:
-                    continue
+        complement_num = target - first_num
+        if complement_num in numbers:
+            if complement_num == first_num and len(numbers[first_num]) < 2:
+                continue
             first_idx = numbers[first_num].popleft()
-            second_idx = numbers[complementary_number].popleft()
+            second_idx = numbers[complement_num].popleft()
             return [first_idx, second_idx]
 
