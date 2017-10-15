095629ce28c6a5eab938ec77f91bd9305aa566dc
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2017-10-15 23:43:41 +0100

    MAINT: traverse list containing nums only once in *two_sum* solution

--- problems/tests/test_two_sum.py
+++ problems/tests/test_two_sum.py
@@ -10,7 +10,8 @@ from problems.two_sum import two_sum
     ([3, 2, 4], 6, [1, 2]),
     ([3, 3], 6, [0, 1]),
     ([-3, 4, 3, 90], 0, [0, 2]),
-    ([5, 75, 25], 100, [1, 2])
+    ([5, 75, 25], 100, [1, 2]),
+    ([5, 5, 5], 10, [0, 1])
 ])
 def test_two_sum(nums, target, expected):
     assert two_sum(nums=nums, target=target) == expected
--- problems/two_sum.py
+++ problems/two_sum.py
@@ -7,8 +7,7 @@ def two_sum(nums, target):
     they add up to a specific target.
 
     Notes: O(n) time complexity. We traverse the list containing n elements
-        exactly twice. Since the hash table reduces the look up time to O(1),
-        the time complexity is O(n).
+        only once. Each look up in the table costs only O(1) time.
 
     Args:
         nums (list): The list of integers to scan.
@@ -17,20 +16,17 @@ def two_sum(nums, target):
     Returns:
         (list) The indexes of the elements that sum to the target value.
     """
-    # convert `target` to dictionary for fast search
+    # convert `target` to dictionary to loop up in O(1) time
     num_to_idx = defaultdict(deque)
-    for idx, number in enumerate(nums):
-        num_to_idx[number].append(idx)
 
-    # find complement number to sum up to target
-    for first_num in nums:
-        complement_num = target - first_num
+    for idx, number in enumerate(nums):
+        complement_num = target - number
         if complement_num in num_to_idx:
-            if complement_num == first_num and len(num_to_idx[first_num]) < 2:
-                continue
-            first_idx = num_to_idx[first_num].popleft()
-            second_idx = num_to_idx[complement_num].popleft()
-            return [first_idx, second_idx]
+            first_idx = num_to_idx[complement_num].popleft()
+            return [first_idx, idx]
+
+        # add to dictionary for future search
+        num_to_idx[number].append(idx)
 
     raise ValueError('No two sum solution.')
 
