4831b59db6525412e490ce2e64c18ff236a5c23e
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2017-10-16 00:08:20 +0100

    STY: cosmetic changes

--- problems/tests/test_two_sum.py
+++ problems/tests/test_two_sum.py
@@ -4,7 +4,7 @@ from pytest import raises
 from problems.two_sum import two_sum
 
 
-@pytest.mark.parametrize('nums,target,expected', [
+@pytest.mark.parametrize('numbers,target,expected', [
     ([2, 7, 11, 15], 9, [0, 1]),
     ([11, 15, 2, 7], 9, [2, 3]),
     ([3, 2, 4], 6, [1, 2]),
@@ -13,10 +13,10 @@ from problems.two_sum import two_sum
     ([5, 75, 25], 100, [1, 2]),
     ([5, 5, 5], 10, [0, 1])
 ])
-def test_two_sum(nums, target, expected):
-    assert two_sum(nums=nums, target=target) == expected
+def test_two_sum(numbers, target, expected):
+    assert two_sum(numbers=numbers, target=target) == expected
 
 def test_two_sum_error():
     with pytest.raises(ValueError):
-        two_sum(nums=[1, 1, 5], target=10)
+        two_sum(numbers=[1, 1, 5], target=10)
 
--- problems/two_sum.py
+++ problems/two_sum.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from collections import deque
 
 
-def two_sum(nums, target):
+def two_sum(numbers, target):
     """Given an array of integers, return indices of the two numbers such that
     they add up to a specific target.
 
@@ -10,23 +10,18 @@ def two_sum(nums, target):
         only once. Each look up in the table costs only O(1) time.
 
     Args:
-        nums (list): The list of integers to scan.
+        numbers (list): The list of integers to scan.
         target (int): The target.
 
     Returns:
         (list) The indexes of the elements that sum to the target value.
     """
-    # convert `target` to dictionary to loop up in O(1) time
-    num_to_idx = defaultdict(deque)
-
-    for idx, number in enumerate(nums):
-        complement_num = target - number
-        if complement_num in num_to_idx:
-            first_idx = num_to_idx[complement_num].popleft()
-            return [first_idx, idx]
-
-        # add to dictionary for future search
-        num_to_idx[number].append(idx)
-
-    raise ValueError('No two sum solution.')
+    already_seen = defaultdict(deque)
+    for idx, number in enumerate(numbers):
+        complement = target - number
+        if complement in already_seen:
+            complement_idx = already_seen[complement].popleft()
+            return [complement_idx, idx]
+        already_seen[number].append(idx)
+    raise ValueError('No solution found')
 
